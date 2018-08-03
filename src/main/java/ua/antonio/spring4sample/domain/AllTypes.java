7c01fe7a3af5e95afbb53938356f9dab68078c5b
Anton Vyshnevskyi <anton_vyshnevskyi@epam.com>
2018-08-03 11:46:55 +0300

    Spring Converters chack

---
+++ src/main/java/ua/antonio/spring4sample/domain/AllTypes.java
@@ -0,0 +1,86 @@
+package ua.antonio.spring4sample.domain;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+/**
+ * Sample of Spring converters usage
+ */
+public class AllTypes {
+    private String p1;
+
+    private boolean p2;
+
+    private byte p3;
+    private short p4;
+    private int p5;
+    private long p6;
+
+    private Integer p7;
+
+    private List p8;
+    private Set p9;
+    private Map p10;
+
+    private Properties p11;
+    private Properties p12;
+
+    private String p13;
+    private String p14;
+    private String p15;
+    private String p16;
+    private String p17;
+    private String p18;
+    private String p19;
+    private String p20;
+
+    public void setP1(String p1) {
+        this.p1 = p1;
+    }
+
+    public void setP2(boolean p2) {
+        this.p2 = p2;
+    }
+
+    public void setP3(byte p3) {
+        this.p3 = p3;
+    }
+
+    public void setP4(short p4) {
+        this.p4 = p4;
+    }
+
+    public void setP5(int p5) {
+        this.p5 = p5;
+    }
+
+    public void setP6(long p6) {
+        this.p6 = p6;
+    }
+
+    public void setP7(Integer p7) {
+        this.p7 = p7;
+    }
+
+    public void setP8(List p8) {
+        this.p8 = p8;
+    }
+
+    public void setP9(Set p9) {
+        this.p9 = p9;
+    }
+
+    public void setP10(Map p10) {
+        this.p10 = p10;
+    }
+
+    public void setP11(Properties p11) {
+        this.p11 = p11;
+    }
+
+    public void setP12(Properties p12) {
+        this.p12 = p12;
+    }
+}
---
+++ src/main/java/ua/antonio/spring4sample/domain/AllTypesEntry.java
@@ -0,0 +1,9 @@
+package ua.antonio.spring4sample.domain;
+
+public class AllTypesEntry {
+    private AllTypes allTypesBean;
+
+    public AllTypesEntry(AllTypes allTypesBean) {
+        this.allTypesBean = allTypesBean;
+    }
+}
