c9a5a1df753bbad10808254831cdccffc071a250
Anton Vyshnevskyi <anton_vyshnevskyi@epam.com>
2018-08-03 10:52:24 +0300

    properties

--- src/main/java/ua/antonio/spring4sample/repository/JdbcUserRepo.java
+++ src/main/java/ua/antonio/spring4sample/repository/JdbcUserRepo.java
@@ -1,11 +1,14 @@
 package ua.antonio.spring4sample.repository;
 
+import com.mysql.jdbc.Driver;
 import org.springframework.jdbc.core.JdbcTemplate;
 import ua.antonio.spring4sample.domain.User;
 
 import javax.sql.DataSource;
 
 public class JdbcUserRepo implements UserRepo {
+    public static String DRIVER_NAME = Driver.class.getName();
+
     private JdbcTemplate jdbcTemplate;
 
     public JdbcUserRepo(DataSource dataSource) {
