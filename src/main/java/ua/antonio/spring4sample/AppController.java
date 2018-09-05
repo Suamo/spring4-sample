4f2edd6d50c79ee16b24d47ead36f1344699c35c
Anton Vyshnevskyi <anton_vyshnevskyi@epam.com>
2018-09-05 17:44:45 +0300

    Simple Spring MVC + tymeleaf (without custom Config, ViewResolver, ServletContext)

--- src/main/java/ua/antonio/spring4sample/AppController.java
+++ src/main/java/ua/antonio/spring4sample/AppController.java
@@ -2,31 +2,30 @@ package ua.antonio.spring4sample;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/users")
 public class AppController {
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String list() {
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public String list(Model model) {
+        model.addAttribute("users", new String[]{"Valera", "Alena", "Zinaida"});
         return "list";
     }
 
-
     // http://localhost:8080/users/showUser?userId=105
-    @RequestMapping(value = "/showUser", method = RequestMethod.GET)
+    @GetMapping(value = "/showUser")
     public String showUserByRequestParam(@RequestParam("userId") Long id, Model model) {
-        return "";
+        model.addAttribute("users", new String[]{"Valera?" + id});
+        return "list";
     }
 
     // http://localhost:8080/users/105
     @RequestMapping(value = "/{userId}", method = RequestMethod.GET)
     public String showUserByPathVariable(@PathVariable("userId") Long id, Model model) {
-        return "";
+        model.addAttribute("users", new String[]{"Valera/" + id});
+        return "list";
     }
 
 }
--- src/main/java/ua/antonio/spring4sample/BootApplication.java
+++ src/main/java/ua/antonio/spring4sample/BootApplication.java
@@ -2,28 +2,12 @@ package ua.antonio.spring4sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.servlet.DispatcherServlet;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
 
 @SpringBootApplication
-public class BootApplication implements WebApplicationInitializer {
+public class BootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootApplication.class, args);
-	}
-
-
-	@Override
-	public void onStartup(ServletContext servletContext) throws ServletException {
-		ServletRegistration.Dynamic registration = servletContext.addServlet("dispatcher", new DispatcherServlet());
-		registration.setLoadOnStartup(1);
-		registration.addMapping("/");
-		registration.setInitParameter("contextConfigLocation", "ua.antonio.spring4sample.config.web.WebConfig");
-		registration.setInitParameter("contextClass", "o.s.w.c.s.AnnotationConfigWebApplicationContext");
+		SpringApplication.run(BootApplication.class);
 	}
 
 }
