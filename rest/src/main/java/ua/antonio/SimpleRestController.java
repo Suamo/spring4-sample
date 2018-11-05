fc2cdf2d2b1c0f1ec8f58beff03cf60f43ae6b1e
Anton Vyshnevskyi <forvao@gmail.com>
2018-11-05 20:58:53 +0200

    Exception handling samples

--- rest/src/main/java/ua/antonio/SimpleRestController.java
+++ rest/src/main/java/ua/antonio/SimpleRestController.java
@@ -7,23 +7,50 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/users")
 public class SimpleRestController {
 
-    /**
-     * Maps IllegalArgumentExceptions to a 404 Not Found HTTP status code.
-     */
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler({IllegalArgumentException.class})
+    @ResponseStatus(HttpStatus.VARIANT_ALSO_NEGOTIATES)
+    @ExceptionHandler({MyException.class})
     public void handleNotFound() {
-        System.out.println("-<>- SimpleRestController.handleNotFound");
+        System.out.println("-<>- Intercepting all MyException and returning 506 status code to client side.");
     }
 
     @GetMapping(value = "/list")
     public String list() {
-        throw new IllegalArgumentException("Ooops");
+        throw new MyException("Ooops");
     }
 
     @GetMapping(value = "/list2")
     public String list2() {
-        throw new RuntimeException("Ooops2");
+        throw new MyException2();
     }
 
+    @ResponseStatus(HttpStatus.EXPECTATION_FAILED)
+    @GetMapping(value = "/list3")
+    public void list3() {
+        System.out.println("Just returning 417 status code for /list3 requests");
+    }
+
+    @GetMapping(value = "/list4")
+    public void list4() {
+        throw new MyException3("Intercepting MyException3 with @ControllerAdvice and returning 409 status code to client side.");
+    }
+
+
+    class MyException extends IllegalArgumentException {
+        MyException(String s) {
+            super(s);
+        }
+    }
+
+    @ResponseStatus(HttpStatus.I_AM_A_TEAPOT)
+    class MyException2 extends IllegalArgumentException {
+        MyException2() {
+            super("Intercepting all MyException2 and returning 418 status code to client side.");
+        }
+    }
+
+    public static class MyException3 extends IllegalArgumentException {
+        MyException3(String s) {
+            super(s);
+        }
+    }
 }
